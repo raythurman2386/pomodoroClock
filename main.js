21a34b211efd338c3bd0b7af7e2d72bcf824aaa7
Raymond Thurman <raymondthurman5@gmail.com>
2019-03-12 20:01:38 -0400

    break countdown implemented
    rough draft of the countdown timer.

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ if (status === true) {
 } else {
   heading.textContent = "BREAK TIME!!!";
 }
+
+heading.addEventListener("click", startClock);
+
+function startClock(e) {
+  let duration = 60 * 5;
+  let timer = duration,
+    minutes,
+    seconds;
+  setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    liveSession.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+// have a way to start the clock
+
+// have a way to reset the clock
+
+// set up the countdown to work
